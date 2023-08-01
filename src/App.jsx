85fb4f3b4b09e81a43317d95297526bb003cdda9
Facundo Gomez <facu1131@gmail.com>
2023-07-31 21:55:05 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      App
+      <NavBar />
+
+    </div>
+  )
+}
+
+export default App
